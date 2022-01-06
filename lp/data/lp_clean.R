237d38c3d0f0199ab26e664eb6319912d1cd21b8
muyi12344 <muyi12344@outlook.com>
2022-01-06 15:29:11 -0500

    LP updated

--- lp/data/lp_clean.R
+++ lp/data/lp_clean.R
@@ -2,7 +2,9 @@
 #
 # Qian Wu
 # Local Projection
-# ==============================================================================================
+# ===========================================================================================================================
+#                                                  House Keeping
+# ===========================================================================================================================
 # Set work directory
 rm(list = ls())
 WORK.DIR = "/Users/mac/Desktop/Research/sofr/lp/data"
@@ -28,11 +30,18 @@ library(tidyverse)
 
 
 
-# Read Shock Data
+# ===========================================================================================================================
+#                                        Identify Govt Borrowing Shock
+# ===========================================================================================================================
+
+# Read Data
 ip=read.csv('ip.csv')
 stock=read.csv('SP500.csv')
 debt=read.csv('debt.csv')
+treasury_KS=read.csv('../../Klingler and Syrstad (2021)/ReplicateDaily.csv')
+treasury_IS=read.csv('../../Infante and Saravay (2020)/replication/tsyOutData.csv')
 
+# Data Cleaning
 debt$Date=as.Date(debt$Date, format="%m/%d/%y" )
 debt$month=factor(months(debt$Date), levels=month.name)
 debt$year=format(debt$Date, format="%Y")
@@ -67,77 +76,119 @@ names(stock_m)[3]="SP500"
 stock_m=stock_m[stock_m$year>2002 & stock_m$year<2020, ]
 stock_m=arrange(stock_m, year, month)
 
+treasury_KS=treasury_KS[,-1]
+treasury_KS$Date=as.Date(treasury_KS$Date)
+names(treasury_KS)[8]="KStsy"
+treasury_KS$month=factor(months(treasury_KS$Date), levels=month.name)
+treasury_KS$year=format(treasury_KS$Date, format="%Y")
+treasury_KS_m=aggregate(as.numeric(KStsy)~year+month, treasury_KS, mean)
+treasury_KS_m=arrange(treasury_KS_m, year, month)
+treasury_KS_m=treasury_KS_m[treasury_KS_m$year>2002 & treasury_KS_m$year<2020 ,]
+names(treasury_KS_m)[3]="KStsy"
 
+# Merge Shock Data
+shock_data=merge(debt_m, ip_m, by=c("year", "month"), all=T)
+shock_data=merge(shock_data, stock_m, by=c("year", "month"), all=T)
+shock_data=merge(shock_data, treasury_KS_m, by=c("year", "month"), all=T)
+shock_data=arrange(shock_data, year, month)
+shock_data=shock_data[-1,]
+
+# Identify Borrowing Shock
+shock_reg_debt=lm(log(debt)~log(lag(debt, 1))+log(lag(debt, 2))+log(lag(debt, 3))+log(lag(debt, 4))+log(lag(debt, 5))+log(lag(debt, 6))+
+               log(lag(IP, 1))+log(lag(IP, 2))+log(lag(IP, 3))+log(lag(IP, 4))+log(lag(IP, 4))+log(lag(IP, 5))+log(lag(IP, 6))+
+               log(lag(SP500, 1))+log(lag(SP500, 2))+log(lag(SP500, 3))+log(lag(SP500, 4))+log(lag(SP500, 5))+log(lag(SP500, 6))+
+               c(1:dim(shock_data)[1])+c(1:dim(shock_data)[1])^2, shock_data)
+debt_shock=shock_reg_debt$residuals
+plot(density(debt_shock))
+shock_data$debt_shock=0
+shock_data$debt_shock[7:dim(shock_data)[1]]=debt_shock
+
+shock_data_KStsy=shock_data[is.na(shock_data$KStsy)==0, ]
+shock_reg_KStsy=lm(KStsy~lag(KStsy, 1)+lag(KStsy, 2)+lag(KStsy, 3)+
+               log(lag(IP, 1))+log(lag(IP, 2))+log(lag(IP, 3))+
+               log(lag(SP500, 1))+log(lag(SP500, 2))+log(lag(SP500, 3))+
+               c(1:dim(shock_data_KStsy)[1])+c(1:dim(shock_data_KStsy)[1])^2, shock_data_KStsy)
+KStsy_shock=shock_reg_KStsy$residuals
+plot(density(KStsy_shock))
+shock_data$KStsy_shock=0
+shock_data$KStsy_shock[142:dim(shock_data)[1]]=KStsy_shock
 
 # Save Shock Data
-shock_data=merge(debt_m, ip_m, by=c("year", "month"))
-shock_data=merge(shock_data, stock_m, by=c("year", "month"))
-shock_data=arrange(shock_data, year, month)
-write_csv(shock_data, "shock_data.csv")
-
-
-
-# Identify Debt Shock
-shock_reg=lm(log(debt)~log(lag(debt, 1))+log(lag(debt, 2))+log(lag(debt, 3))+log(lag(debt, 4))+
-               log(lag(IP, 1))+log(lag(IP, 2))+log(lag(IP, 3))+log(lag(IP, 4))+
-               log(lag(SP500, 1))+log(lag(SP500, 2))+log(lag(SP500, 3))+log(lag(SP500, 4))+
-               c(1:203), shock_data)
-debt_shock=shock_reg$residuals
+write.csv(shock_data, "shock_data.csv", row.names=FALSE)
 
 
 
 
-# Read Local Projection Data
-sofr=read.csv('sofr.csv')
-libor=read.csv('libor.csv')
-ffr=read.csv('ffr.csv')
+# ===========================================================================================================================
+#                                        Prepare Local Projection Data
+# ===========================================================================================================================
 
-sofr$Date=as.Date(sofr$Date, format="%m/%d/%y")
-sofr=sofr[,1:2]
-names(sofr)=c('Date', 'SOFR')
-sofr$SOFR=as.numeric(sofr$SOFR)
-sofr$SOFR[c(900:1841)]=100*sofr$SOFR[c(900:1841)]
-sofr$month=factor(months(sofr$Date), levels=month.name)
-sofr$year=format(sofr$Date, format="%Y")
-sofr_m=aggregate(SOFR~year+month, sofr, mean)
-sofr_m=sofr_m[sofr_m$year>2002 & sofr_m$year<2020, ]
-sofr_m=arrange(sofr_m, year, month)
+# Prepare Endogenous Data
+lp_data=read.csv('../../ols/data/Master data/data_reg2_KS.csv')
+lp_data=data.frame(Date=lp_data$Date, SOFR=lp_data$SOFR, LIBOR=lp_data$LIBOR, FFR=lp_data$FFR, index=lp_data$index)
+lp_data$Date=as.Date(lp_data$Date)
+lp_data$month=factor(months(lp_data$Date), levels=month.name)
+lp_data$year=format(lp_data$Date, format="%Y")
 
-libor$DATE=as.Date(libor$DATE)
-names(libor)=c('Date', 'LIBOR')
-libor$LIBOR=as.numeric(libor$LIBOR)
-libor$LIBOR=libor$LIBOR*100
-libor$month=factor(months(libor$Date), levels=month.name)
-libor$year=format(libor$Date, format="%Y")
-libor_m=aggregate(LIBOR~year+month, libor, mean)
-libor_m=libor_m[libor_m$year>2002 & libor_m$year<2020, ]
-libor_m=arrange(libor_m, year, month)
+# Monthly Average with Outliers
+lp_data_out=aggregate(cbind(SOFR, LIBOR, FFR)~year+month, lp_data, mean)
+lp_data_out=lp_data_out[lp_data_out$year>2002 & lp_data_out$year<2020, ]
+lp_data_out=arrange(lp_data_out, year, month)
 
-ffr$DATE=as.Date(ffr$DATE)
-names(ffr)=c('Date', 'FFR')
-ffr$FFR=as.numeric(ffr$FFR)*100
-ffr$month=factor(months(ffr$Date), levels=month.name)
-ffr$year=format(ffr$Date, format="%Y")
-ffr_m=aggregate(FFR~year+month, ffr, mean)
-ffr_m=ffr_m[ffr_m$year>2002 & ffr_m$year<2020,]
-ffr_m=arrange(ffr_m, year, month)
+# Monthly Average without Outliers
+lp_data_noout=aggregate(cbind(SOFR, LIBOR, FFR)~year+month, lp_data[lp_data$index==1,], mean)
+lp_data_noout=lp_data_noout[lp_data_noout$year>2002 & lp_data_noout$year<2020, ]
+lp_data_noout=arrange(lp_data_noout, year, month)
 
 
-
-# Save Local Projection Data
-lp_data=merge(libor_m, sofr_m, by=c("year", "month"))
-lp_data=merge(lp_data, ffr_m, by=c("year", "month"))
-lp_data=merge(lp_data, shock_data, by=c("year", "month"))
-write_csv(lp_data, "lp_data.csv")
+# Save Endogenous Data
+write_csv(lp_data_out, "lp_data_out.csv")
+write_csv(lp_data_noout, "lp_data_noout.csv")
 
 
 
 
+# ===========================================================================================================================
+#                                        Local Projection Regression
+# ===========================================================================================================================
+library(gridExtra)
+library(ggpubr)
+library(lpirfs)
 
+# Read Data
+shock_data=read.csv('shock_data.csv')
+shock_data=shock_data[139:dim(shock_data)[1],]
+end_data_out=read.csv('lp_data_out.csv')
+end_data_noout=read.csv('lp_data_noout.csv')
 
+# Set Variables
+exog_data_debt=data.frame(log(shock_data[,c(3,4)]))
+exog_data_KStst=data.frame(log(shock_data[,c(4,6)]))
+SOFR_spread=data.frame(SOFR_spread=end_data_noout$SOFR-end_data_noout$FFR)
+LIBOR_spread=data.frame(LIBOR_spread=end_data_noout$LIBOR-end_data_noout$FFR)
+debt_shock=data.frame(shock_data[,7])
+KStsy_shock=data.frame(shock_data[,8])
 
+# Local Projection with IV
+SOFR_iv_debt <- lp_lin_iv(endog_data = SOFR_spread, lags_endog_lin = 3,
+shock = debt_shock, exog_data=exog_data_debt, lags_exog=3,
+trend = 0, confint = 1, hor = 12)
+plot_lin(SOFR_iv_debt)   
 
+LIBOR_iv_debt <- lp_lin_iv(endog_data = LIBOR_spread, lags_endog_lin = 3,
+shock = debt_shock, exog_data=exog_data_debt, lags_exog=3,
+trend = 0, confint = 1, hor = 12)
+plot_lin(LIBOR_iv_debt)   
 
+SOFR_iv_KStsy <- lp_lin_iv(endog_data = data.frame(SOFR_spread[-c(1:3),]), lags_endog_lin = 3,
+shock = data.frame(KStsy_shock[-c(1:3),]), exog_data=data.frame(exog_data_KStst[-c(1:3),]), lags_exog=3,
+trend = 0, confint = 1, hor = 12)
+plot_lin(SOFR_iv_KStsy)   
+
+LIBOR_iv_KStsy <- lp_lin_iv(endog_data = data.frame(LIBOR_spread[-c(1:3),]), lags_endog_lin = 3,
+shock = data.frame(KStsy_shock[-c(1:3),]), exog_data=data.frame(exog_data_KStst[-c(1:3),]), lags_exog=3,
+trend = 0, confint = 1, hor = 12)
+plot_lin(LIBOR_iv_KStsy)   
 
 
 
--- ols/data/ols_clean.R
+++ ols/data/ols_clean.R
@@ -1,8 +1,12 @@
 # Extra Crowding-Out by SOFR
 #
 # Qian Wu
+#
 # OLS
-# ==============================================================================================
+#
+# ===========================================================================================================================
+#                                                  House Keeping
+# ===========================================================================================================================
 # Set work directory
 rm(list = ls())
 WORK.DIR = "/Users/mac/Desktop/Research/sofr/ols/data"
@@ -25,16 +29,26 @@ library(tigris)
 library(reshape2)
 library(lubridate)
 library(tidyverse)
+library(lfe)
 
 
+# ===========================================================================================================================
+#                                        Reg 2: SOFR,LIBOR and Govt Borrowing
+# ===========================================================================================================================
+
+# ------------------------------------------------------------------------------
+# Govt debt outstanding as the borrowing measure 
+# ------------------------------------------------------------------------------
 
 # Read Data
 sofr=read.csv('sofr.csv')
 libor=read.csv('libor.csv')
 debt=read.csv('debt.csv')
 ffr=read.csv('ffr.csv')
-volume=read.csv('volume.csv')
+vol_rate=read.csv('vol_rate.csv')
 
+
+# Clean Data
 debt$Record.Date=as.Date(debt$Record.Date, format="%m/%d/%y" )
 names(debt)=c('Date', 'Public', 'Govt', 'Total')
 
@@ -53,16 +67,13 @@ ffr$DATE=as.Date(ffr$DATE)
 names(ffr)=c('Date', 'FFR')
 ffr$FFR=as.numeric(ffr$FFR)*100
 
-volume=
-volume %>% 
-  separate(Date, c("month", "day", "year"), sep = "/")
-volume$Date<-as.Date(with(volume,paste(month,day,year,sep="/")),"%m/%d/%y")
-volume=volume[, -c(1:3)]
+vol_rate=read.csv('vol_rate.csv')
+vol_rate$Date=as.Date(vol_rate$Date, format="%m/%d/%y")
 
 data=merge(sofr, libor, by="Date")
 data=merge(data, ffr, by="Date")
 data=merge(data, debt, by='Date')
-data=merge(data, volume, by="Date")
+data=merge(data, vol_rate, by="Date", all=T)
 
 log_Total=log(data$Total)
 data$log_Total=log_Total
@@ -80,15 +91,16 @@ data$SOFR_spread_change=c(0, diff(data$SOFR_spread, lag=1))
 data$LIBOR_spread=data$LIBOR-data$FFR
 data$LIBOR_spread_change=c(0, diff(data$LIBOR_spread, lag=1))
 
+data$Volume=data$Tri_Party_vol+data$GC_vol+data$Bilateral_vol
 data$log_Volume=log(data$Volume)
 data$difflog_Volume=c(0, diff(data$log_Volume, lag=1))
 
-data_reg=data[-c(1, 1263:1267), ]
+data_reg=data
 
 
-
-# Data Visualization
-meltdf <- melt(data_reg[, c(1, 16, 18)],id="Date")
+# SOFR and LIBOR Visualization
+data_reg=data_reg[(1-(data_reg$Date>="2019-9-16" & data_reg$Date<="2019-9-20")==1), ]
+meltdf <- melt(data_reg[, c(1, 18, 20)],id="Date")
 ggplot(meltdf, aes(x=Date, y=value, color=variable, group=variable)) +
   geom_line(size=1)+
   scale_x_date(limit=c(as.Date("2014-08-01"),as.Date("2020-01-01")),
@@ -108,52 +120,144 @@ ggplot(meltdf, aes(x=Date, y=value, color=variable, group=variable)) +
   )
 
 
+# Indicate Outliers
+data_reg=data
+#   the week 9/16/2019-9/20/2019
+index1=(data_reg$Date>="2019-9-16" & data_reg$Date<="2019-9-20")==FALSE
+index1=as.numeric(index1)
 
-
-# Remove the first, last, and second-last days in each month
+#   the first, last, and second-last days in each month
+index2=rep(1, dim(data_reg)[1])
 first=setDT(data_reg)[order(Date), .(Date[which.min(Date)]), 
                 by=.(year(Date), month(Date))]
 index_first=match(first$V1, data_reg$Date)[-1]
-data_reg=data_reg[-index_first, ]
+index2[index_first]=0
 
 last=setDT(data_reg)[order(Date), .( Date[which.max(Date)]), 
                by=.(year(Date), month(Date))]
 index_last=match(last$V1, data_reg$Date)
-data_reg=data_reg[-index_last, ]
+index2[index_last]=0
 
-last=setDT(data_reg)[order(Date), .( Date[which.max(Date)]), 
+last=setDT(data_reg[index2==1,])[order(Date), .( Date[which.max(Date)]), 
                      by=.(year(Date), month(Date))]
-index_last=match(last$V1, data_reg$Date)
-data_reg=data_reg[-index_last, ]
+index_last2=match(last$V1, data_reg$Date)
+index2[index_last2]=0
 
-write_dta(data_reg, 'data_reg.dta')
 
+# combine index1 and index2
+index=index1*index2
+data_reg$index=index
+
+
+# Save data
+write_dta(data_reg, 'Master data/data_reg2_debt.dta')
+write.csv(data_reg, 'Master data/data_reg2_debt.csv', row.names=F)
 
 
 # Regression 2
-reg2=lm(SOFR_spread_change~change_Public, data=data_reg)
-reg2_sum1=coeftest(reg2, vcov.=NeweyWest(reg2, lag=10, prewhite=FALSE, adjust=TRUE, verbose=TRUE))
-reg2_2=lm(SOFR_spread_change~change_Public+lag(SOFR_spread_change), data=data_reg)
-reg2_sum2=coeftest(reg2_2, vcov.=NeweyWest(reg2_2, lag=10, prewhite=FALSE, adjust=TRUE, verbose=TRUE))
-reg2_3=lm(LIBOR_spread_change~change_Public, data=data_reg)
-reg2_sum3=coeftest(reg2_3, vcov.=NeweyWest(reg2_3, lag=10, prewhite=FALSE, adjust=TRUE, verbose=TRUE))
-reg2_4=lm(LIBOR_spread_change~change_Public+lag(LIBOR_spread_change), data=data_reg)
-reg2_sum4=coeftest(reg2_4, vcov.=NeweyWest(reg2_4, lag=10, prewhite=FALSE, adjust=TRUE, verbose=TRUE))
-fit1_r2=round(summary(reg2)$adj.r.squared, 4)
-fit2_r2=round(summary(reg2_2)$adj.r.squared, 4)
-fit3_r2=round(summary(reg2_3)$adj.r.squared, 4)
-fit4_r2=round(summary(reg2_4)$adj.r.squared, 4)
-stargazer(reg2_sum1, reg2_sum2, reg2_sum3, reg2_sum4, type="latex",
-          align = T, dep.var.labels=c("SOFR", "LIBOR"),
-          covariate.labels=c("Government debt", "Lagged SOFR", "Lagged LIBOR"),
-          column.sep.width = "1pt", no.space = T,
-          add.lines = list(c("Adjusted $R^2$", fit1_r2, fit2_r2, fit3_r2, fit4_r2), c("Observations", 1135, 1134, 1100, 1082)))
+data_reg=read.csv('Master data/data_reg2_debt.csv')
+reg2_debt=felm(SOFR_spread_change~change_Public, data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt, robust=T)
+reg2_debt_2=felm(SOFR_spread_change~change_Public+lag(SOFR_spread_change,1), data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_2, robust=T)
+reg2_debt_3=felm(LIBOR_spread_change~change_Public, data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_3, robust=T)
+reg2_debt_4=felm(LIBOR_spread_change~change_Public+lag(LIBOR_spread_change,1), data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_4, robust=T)
+
+stargazer(reg2_debt,reg2_debt_2,reg2_debt_3,reg2_debt_4, type="latex",
+          align=T, dep.var.labels=c("SOFR", "LIBOR"), 
+          no.space = T)
 
 
 
 
-# Data for Reg1
+# ------------------------------------------------------------------------------
+# Treasuries outstanding (Klingler and Syrstad (2021)) as the borrowing measure 
+# ------------------------------------------------------------------------------
+
+# Read Data
+data_reg2_KS=read.csv('../../Klingler and Syrstad (2021)/ReplicateDaily.csv')
+data_reg=read.csv('Master data/data_reg2_debt.csv')
+data_reg$Date=as.Date(data_reg$Date)
+
+# Clean Data
+data_reg2_KS=data_reg2_KS[,-1]
+data_reg2_KS$Date=as.Date(data_reg2_KS$Date)
+data_reg=merge(data_reg, data_reg2_KS[,c(1, 8)], by="Date", all=T)
+names(data_reg)[26]="log_KStsy"
+data_reg$difflog_KStsy=c(0, diff(data_reg$log_KStsy,1))
+
+
+# Save data
+write_dta(data_reg, 'Master data/data_reg2_KS.dta')
+write.csv(data_reg, 'Master data/data_reg2_KS.csv', row.names = F)
+
+
+# Regression 2
+data_reg=read.csv('Master data/data_reg2_KS.csv')
+reg2_debt=felm(SOFR_spread_change~difflog_KStsy, data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt, robust=T)
+reg2_debt_2=felm(SOFR_spread_change~difflog_KStsy+lag(SOFR_spread_change,1), data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_2, robust=T)
+reg2_debt_3=felm(LIBOR_spread_change~difflog_KStsy, data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_3, robust=T)
+reg2_debt_4=felm(LIBOR_spread_change~difflog_KStsy+lag(LIBOR_spread_change,1), data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_4, robust=T)
+
+stargazer(reg2_debt,reg2_debt_2,reg2_debt_3,reg2_debt_4, type="latex",
+          align=T, dep.var.labels=c("SOFR", "LIBOR"), 
+          no.space = T)
+
+
+
+# ------------------------------------------------------------------------------
+# Treasuries outstanding (Infante and Saravay (2020)) as the borrowing measure 
+# ------------------------------------------------------------------------------
+
+# Read Data
+data_reg2_IS=read.csv('../../Infante and Saravay (2020)/replication/tsyOutData.csv')
+data_reg=read.csv('Master data/data_reg2_KS.csv')
+data_reg$Date=as.Date(data_reg$Date)
+
+# Clean Data
+data_reg2_IS$IStsy=data_reg2_IS$TbillsOut+data_reg2_IS$ShTbillsOut+data_reg2_IS$USTNotesOut
+names(data_reg2_IS)[1]="Date"
+data_reg2_IS$Date=as.Date(data_reg2_IS$Date)
+data_reg=merge(data_reg, data_reg2_IS[, c(1,5)], by="Date")
+data_reg$log_IStsy=log(data_reg$IStsy)
+data_reg$difflog_IStsy=c(0, diff(data_reg$log_IStsy,1))
+
+# Save Data
+write_dta(data_reg, 'Master data/data_reg2_IS.dta')
+write.csv(data_reg, 'master data/data_reg2_IS.csv', row.names = F)
+
+# Regression 2
+data_reg=read.csv('master data/data_reg2_IS.csv')
+reg2_debt=felm(SOFR_spread_change~difflog_IStsy, data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt, robust=T)
+reg2_debt_2=felm(SOFR_spread_change~difflog_IStsy+lag(SOFR_spread_change,1), data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_2, robust=T)
+reg2_debt_3=felm(LIBOR_spread_change~difflog_IStsy, data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_3, robust=T)
+reg2_debt_4=felm(LIBOR_spread_change~difflog_IStsy+lag(LIBOR_spread_change,1), data=data_reg[data_reg$index==1,], na.action=na.exclude)
+summary(reg2_debt_4, robust=T)
+
+
+
+
+# ===========================================================================================================================
+#                              Reg 1: Primary Dealers Repo Volume and Govt Borrowing
+# ===========================================================================================================================
+
+# ------------------------------------------------------------------------------
+# Govt debt outstanding as the borrowing measure 
+# ------------------------------------------------------------------------------
+
+# Read Data
 pri_repo=read.csv('primary_repo.csv')
+
+# Clean Data
 pri_repo$date=as.Date(pri_repo$date)
 names(pri_repo)=c('Date', 'repo')
 data_week=merge(pri_repo, debt, by="Date")
@@ -161,26 +265,25 @@ data_week$log_repo=log(data_week$repo)
 data_week$change_repo=c(0, diff(data_week$log_repo, lag=1))
 data_week$log_public=log(data_week$Public)
 data_week$change_public=c(0, diff(data_week$log_public))
-write_dta(data_week, 'data_week.dta')
 
+# Save Data
+write_dta(data_week, 'Master data/data_reg1_debt.dta')
+write.csv(data_week, 'Master data/data_reg1_debt.csv')
 
 
 # Regression 1
-reg1=lm(change_repo~change_public, data=data_week)
-reg1_sum1=coeftest(reg1, vcov.=NeweyWest(reg1, lag=4, prewhite=FALSE, adjust=TRUE, verbose=TRUE))
-reg1_2=lm(change_repo~change_public+lag(change_repo), data=data_week)
-reg1_sum2=coeftest(reg1_2, vcov.=NeweyWest(reg1_2, lag=4, prewhite=FALSE, adjust=TRUE, verbose=TRUE))
-fit1_r2=round(summary(reg1)$adj.r.squared, 4)
-fit2_r2=round(summary(reg1_2)$adj.r.squared, 4)
-stargazer(reg1_sum1,reg1_sum2, type="latex", title="Effect of Government Debt on Primary Dealers' Repo Activity",
-          align=T, dep.var.labels=c("Primary dealers' Repo"),
-          covariate.labels=c("Government debt", "Lagged primary dealers' Repo"),
-          add.lines = list(c("Adjusted $R^2$", fit1_r2, fit2_r2), c("Observations", 351, 350)),
-          column.sep.width = "1pt", no.space = T)
+reg1_debt=felm(change_repo~change_public, data=data_week)
+summary(reg1_debt, robust=T)
 
 
 
-# Business Debt Components Visualization
+
+
+
+# ===========================================================================================================================
+#                              Business Debt Decomposition
+# ===========================================================================================================================
+
 business=read.csv('business.csv')
 business[, -1]=business[, -1]/1000
 business$year=str_sub(business$date, 1,4)
@@ -208,8 +311,58 @@ ggplot(meltdf, aes(x=date, y=value, fill=variable)) +
 
 
 
+# ===========================================================================================================================
+#                              SOFR Underlying Transactions Decomposition
+# ===========================================================================================================================
+data_reg=read.csv('Master data/data_reg2_debt.csv')
+data_reg$Date=as.Date(data_reg$Date)
+volume=data_reg[, c(1,8,9,10)]
+volume$GC=volume$Tri_Party_vol+volume$GC_vol
+volume$SC=volume$Bilateral_vol
+
+meltdf <- melt(volume[,c(1,5,6)],id="Date")
+meltdf=na.exclude(meltdf)
+names(meltdf)[2]="Transaction_Type"
+meltdf=arrange(meltdf, Date,Transaction_Type )
+ggplot(meltdf, aes(x=Date, y=value, fill=Transaction_Type)) + 
+  geom_area()+
+  ylab("billions USD")+
+  xlab("")+
+  theme(panel.background = element_rect(fill = NA),
+        panel.grid.major = element_line(colour = "grey90"),
+        legend.position = c(.05, .95),
+        legend.justification = c("left", "top"),
+        legend.box.just = "left",
+        legend.margin = margin(6, 6, 6, 6),
+        axis.title=element_text(size=14),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14)
+  )
 
 
 
+# ===========================================================================================================================
+#                              Reg 3: Bilateral Rate and Govt Borrowing
+# ===========================================================================================================================
 
+data=read.csv('Master data/data_reg2_KS.csv')
+rate=data[, c(1,4,8:13,17,25,27)]
+rate$GC_rate=(rate$Tri_Party_vol*rate$Tri_Party_rate+rate$GC_vol*rate$GC_rate)/(rate$Tri_Party_vol+rate$GC_vol)
+rate$SC_rate=rate$Bilateral_rate
+lines(rate$Date, rate$SC_rate)
+rate$GC_rate_change=c(0, diff(rate$GC_rate_spread, 1))
+rate$SC_rate_change=c(0, diff(rate$SC_rate_spread, 1))
+reg3=felm(SC_rate_change-GC_rate~difflog_KStsy, rate[rate$index==1,], na.action = na.exclude)
+summary(reg3, robust=T)
+rate$log_GC_vol=log(rate$Tri_Party_vol+rate$GC_vol)
+rate$log_SC_vol=log(rate$Bilateral_vol)
+rate$difflog_GC_vol=c(0, diff(rate$log_GC_vol))
+rate$difflog_SC_vol=c(0, diff(rate$log_SC_vol))
+reg3_2=felm(GC_rate_spread_change-SC_rate_spread_change~difflog_KStsy+difflog_GC_vol+difflog_SC_vol, rate, na.action = na.exclude)
+summary(reg3_2, robust=T)
+
+
+stargazer(reg3,reg3_2, type="latex",
+          align=T, dep.var.labels=c('SC repo rate - GC repo rate'), 
+          no.space = T)
 
